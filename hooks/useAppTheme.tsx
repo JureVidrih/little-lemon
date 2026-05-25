63a7e10b11f1a46d9a66eba2415960acbebc10cd
Jure Vidrih <jurevidrih@gmail.com>
2026-05-25 14:51:07 +0200

    Init implement of useAppTheme

---
+++ hooks/useAppTheme.tsx
@@ -0,0 +1,14 @@
+const appTheme = {
+    primary_1: "#495E57",
+    primary_2: "#F4CE14",
+    secondary_1: "#EE9972",
+    secondary_2: "#FBDABB",
+    secondary_3: "#EDEFEE",
+    secondary_4: "#333333"
+};
+
+const useAppTheme = () => {
+    return appTheme;
+}
+
+export { useAppTheme };
