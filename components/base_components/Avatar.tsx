4b7133ed94df429cd099b85d654beedf74d6bb18
Jure Vidrih <jurevidrih@gmail.com>
2026-05-27 19:01:13 +0200

    <Avatar /> now shows initials(if provided) of user, when profile avatar is unavailable

--- components/base_components/Avatar.tsx
+++ components/base_components/Avatar.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, Image, StyleSheet, Pressable } from 'react-native';
+import { View, Image, StyleSheet, Pressable, Text } from 'react-native';
 
 
-import { useAppTheme } from '../../hooks/';
+import { useAppTheme, useSessionStorage } from '../../hooks/';
 
 type AvatarProps = {
     mode?: string,
@@ -17,16 +17,20 @@ export default function({
     }: AvatarProps) {
     const theme = useAppTheme();
 
+    const sessionStorage = useSessionStorage();
+
     const configuration = {
         width: 100,
         height: 100,
-        borderRadius: 50
+        borderRadius: 50,
+        labelSize: 36
     }
 
     if(mode === "header") {
         configuration.width = 60;
         configuration.height = 60;
         configuration.borderRadius = 30;
+        configuration.labelSize = 28;
     }
 
     return (
@@ -36,10 +40,25 @@ export default function({
             evt.stopPropagation();
             onPress?.();
         }}>
-            <Image 
-            style={{ width: '100%', height: '100%' }}
-            source={(source !== undefined && source !== null ? { uri: source } : undefined)}
-            />
+            {
+                source !== undefined && source !== null ? (
+                    <Image 
+                    style={{ width: '100%', height: '100%' }}
+                    source={(source !== undefined && source !== null ? { uri: source } : undefined)}
+                    />
+                ) : (
+                    <View
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                        backgroundColor: '#298dff',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}>
+                        <Text style={[styles.placeholderLabel, { fontSize: configuration.labelSize }]}>{sessionStorage.get("profile/firstName")?.[0]?.toUpperCase()}{sessionStorage.get("profile/lastName")?.[0]?.toUpperCase()}</Text>
+                    </View>
+                )
+            }
         </Pressable>
     );
 }
@@ -48,5 +67,8 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: 'gray',
         overflow: 'hidden'
+    },
+    placeholderLabel: {
+        color: '#ffffff'
     }
 });
