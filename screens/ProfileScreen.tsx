181bb877dcd78c14435dd57e2888caa31445fffd
Jure Vidrih <jurevidrih@gmail.com>
2026-05-29 02:04:26 +0200

    <ProfileScreen /> can now discard and save changes

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { StyleSheet, Text, View, Image, ToastAndroid, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -17,18 +17,9 @@ export default function () {
 
   const { reset, setFirstInitial, setLastInitial } = useAvatarState((state: any) => state);
 
-  const [initialValues, setInitialValues] = useState<
-  {
-    firstName: string | null,
-    lastName: string | null,
-    email: string | null,
-    phoneNumber: string | null
-  }>({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phoneNumber: ""
-  });
+  let initialValues = useRef<any>(null);
+
+  const [changedValues, setChangedValues] = useState<any>(null);
 
   useLayoutEffect(() => {
     (async () => {
@@ -36,13 +27,24 @@ export default function () {
       let lastName = await AsyncStorage.getItem("@little-lemon/profile/lastName");
       let email = await AsyncStorage.getItem("@little-lemon/profile/email");
       let phoneNumber = await AsyncStorage.getItem("@little-lemon/profile/phoneNumber");
+
+      let orderStatuses = await AsyncStorage.getItem("@little-lemon/profile/orderStatuses");
+      let passwordChanges = await AsyncStorage.getItem("@little-lemon/profile/passwordChanges");
+      let specialOffers = await AsyncStorage.getItem("@little-lemon/profile/specialOffers");
+      let newsletter = await AsyncStorage.getItem("@little-lemon/profile/newsletter");
       
-      setInitialValues({
+      initialValues.current = {
         firstName,
         lastName,
         email,
-        phoneNumber
-      });
+        phoneNumber,
+        orderStatuses,
+        passwordChanges,
+        specialOffers,
+        newsletter
+      };
+
+      setChangedValues({...initialValues.current})
     })();
     navigation.setOptions({
       header: () => {
@@ -76,12 +78,12 @@ export default function () {
             }}>
               <Input 
               label="First name"
-              value={initialValues.firstName ?? undefined}
+              value={changedValues?.firstName ?? undefined}
               placeholder="Type your first name here..."
               maxLength={100}
               onChangeText={(newValue) => {
-                setFirstInitial(newValue[0]?.toUpperCase());
-                AsyncStorage.setItem("@little-lemon/profile/firstName", newValue); }}
+                setChangedValues({ ...changedValues, firstName: newValue });
+              }}
               validate={(newValue) => { 
                 const regex = /^[a-zA-Z]+$/;
                 return regex.test(newValue);
@@ -91,12 +93,12 @@ export default function () {
               inputMode="text" />
               <Input 
               label="Last name"
-              value={initialValues.lastName ?? undefined}
+              value={changedValues?.lastName ?? undefined}
               placeholder="Type your last name here..."
               maxLength={100}
               onChangeText={(newValue) => {
-                setLastInitial(newValue[0]?.toUpperCase());
-                AsyncStorage.setItem("@little-lemon/profile/lastName", newValue); }}
+                setChangedValues({ ...changedValues, lastName: newValue });
+              }}
               validate={(newValue) => { 
                 const regex = /^[a-zA-Z]+$/;
                 return regex.test(newValue);
@@ -107,10 +109,10 @@ export default function () {
               inputMode="text" />
               <Input 
               label="Email"
-              value={initialValues.email ?? undefined}
+              value={changedValues?.email ?? undefined}
               placeholder="Type your email here..."
               maxLength={100}
-              onChangeText={(newValue) => { AsyncStorage.setItem("@little-lemon/profile/email", newValue); }}
+              onChangeText={(newValue) => { setChangedValues({ ...changedValues, email: newValue }); }}
               validate={(newValue) => {
                 const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
                 return regex.test(newValue);
@@ -121,17 +123,11 @@ export default function () {
               inputMode="email" />
               <Input 
               label="Phone number"
-              value={mask(initialValues.phoneNumber ?? "", "(999) 999-9999")}
+              value={mask(changedValues?.phoneNumber ?? "", "(999) 999-9999")}
               placeholder="Type your phone number here..."
               maxLength={15}
               onChangeText={(newValue) => {
-                AsyncStorage.setItem("@little-lemon/profile/phoneNumber", newValue);
-                setInitialValues({
-                  firstName: "",
-                  lastName: "",
-                  email: "",
-                  phoneNumber: newValue
-                });
+                setChangedValues({ ...changedValues, phoneNumber: newValue });
               }}
               validate={(newValue) => {
                 const regex = /^(?:\+1\s?)?(?:\(?([2-9][0-8][0-9])\)?[-.\s]?)([2-9][0-9]{2})[-.\s]?([0-9]{4})$/;
@@ -146,10 +142,30 @@ export default function () {
         </View>
         <View style={{ width: '90%' }}>
             <Header sizeType={5}>Email notifications</Header>
-            <Checkbox label="Order statuses" checked={true} />
-            <Checkbox label="Password changes" checked={true} />
-            <Checkbox label="Special offers" checked={true} />
-            <Checkbox label="Newsletter" checked={true} />
+            <Checkbox 
+            label="Order statuses" 
+            checked={changedValues?.orderStatuses === "true"}
+            onSelect={(value: boolean) => {
+              setChangedValues({ ...changedValues, orderStatuses: value+"" });
+            }} />
+            <Checkbox 
+            label="Password changes" 
+            checked={changedValues?.passwordChanges === "true"}
+            onSelect={(value: boolean) => {
+              setChangedValues({ ...changedValues, passwordChanges: value+"" });
+            }} />
+            <Checkbox 
+            label="Special offers" 
+            checked={changedValues?.specialOffers === "true"}
+            onSelect={(value: boolean) => {
+              setChangedValues({ ...changedValues, specialOffers: value+"" });
+            }} />
+            <Checkbox 
+            label="Newsletter" 
+            checked={changedValues?.newsletter === "true"}
+            onSelect={(value: boolean) => {
+              setChangedValues({ ...changedValues, newsletter: value+"" });
+            }} />
         </View>
         <View style={{ width: '90%', marginTop: 30 }}>
             <Button 
@@ -174,10 +190,29 @@ export default function () {
             <View style={{ marginVertical: 30, flexDirection: 'row', justifyContent: 'space-evenly' }}>
             <Button 
             border_8
-            color={"white"}>Discard changes</Button>
+            color={"white"}
+            onPress={() => {
+              setChangedValues({...initialValues.current});
+            }}>Discard changes</Button>
             <Button 
             border_8
-            color={"primary_1"}>Save changes</Button>
+            color={"primary_1"}
+            onPress={() => {
+              setFirstInitial(changedValues?.firstName[0]?.toUpperCase() ?? null);
+              AsyncStorage.setItem("@little-lemon/profile/firstName", changedValues?.firstName ?? "");
+
+              setLastInitial(changedValues?.lastName[0]?.toUpperCase() ?? null);
+              AsyncStorage.setItem("@little-lemon/profile/lastName", changedValues?.lastName ?? "");
+
+              AsyncStorage.setItem("@little-lemon/profile/email", changedValues?.email ?? "");
+              AsyncStorage.setItem("@little-lemon/profile/phoneNumber", changedValues?.phoneNumber ?? "");
+
+              AsyncStorage.setItem("@little-lemon/profile/orderStatuses", changedValues?.orderStatuses ?? "");
+              AsyncStorage.setItem("@little-lemon/profile/passwordChanges", changedValues?.passwordChanges ?? "");
+              AsyncStorage.setItem("@little-lemon/profile/specialOffers", changedValues?.specialOffers ?? "");
+              AsyncStorage.setItem("@little-lemon/profile/newsletter", changedValues?.newsletter ?? "");
+              initialValues.current = {...changedValues};
+            }}>Save changes</Button>
             </View>
         </View>
     </ScrollView>
