09090bdea05ef379ff02c497391e2b002669aca3
Jure Vidrih <jurevidrih@gmail.com>
2026-06-26 03:01:16 +0200

    Implement tests for <Checkbox />

---
+++ src/components/base_components/Checkbox/Checkbox.test.js
@@ -0,0 +1,76 @@
+import { render, userEvent } from '@testing-library/react-native';
+
+import Checkbox from './Checkbox.tsx';
+import checkboxStyles from './CheckboxStyles.ts';
+
+import { useAppTheme } from '../../../hooks/useAppTheme.tsx';
+import textStyles from '../Txt/TextStyles.ts';
+
+describe("Checkbox component", () => {
+    it("should have rendered correctly", async () => {
+        const { getByTestId } = await render(<Checkbox label="Label" />);
+
+        expect(getByTestId("pressableContainer")).toBeOnTheScreen();
+    });
+
+    it("should have rendered correctly with no props", async () => {
+        const theme = useAppTheme();
+
+        const { getByTestId } = await render(<Checkbox label="Label" />);
+
+        expect(getByTestId("pressableContainer")).toHaveStyle({
+            ...checkboxStyles.container
+        });
+
+        expect(getByTestId("viewContainer")).toHaveStyle({
+            ...checkboxStyles.checkboxContainer,
+            borderColor: theme.primary_1,
+            backgroundColor: theme.primary_1
+        });
+
+        expect(getByTestId("animationContainer")).toHaveStyle({
+            opacity: 0
+        });
+
+        expect(getByTestId("textContainer")).toHaveStyle({
+            ...checkboxStyles.label,
+            ...textStyles["pregular"]
+        });
+
+        expect(getByTestId("textContainer")).toHaveTextContent("Label");
+    });
+
+    it("should respect checked prop", async () => {
+        const theme = useAppTheme();
+
+        var { getByTestId } = await render(<Checkbox checked={true} label="Label" />);
+
+        expect(getByTestId("animationContainer")).toHaveStyle({
+            opacity: 1
+        });
+
+        var { getByTestId } = await render(<Checkbox checked={false} label="Label" />);
+
+        expect(getByTestId("animationContainer")).toHaveStyle({
+            opacity: 0
+        });
+    });
+
+    it("should handle onPress event properly", async () => {
+        const user = userEvent.setup({
+            delay: 100
+        });
+
+        let checkOnSelect = null;
+
+        var { getByTestId } = await render(<Checkbox onSelect={(checked) => {
+            checkOnSelect = checked;
+        }} label="Label" />);
+
+        await user.press(getByTestId("pressableContainer"));
+        expect(checkOnSelect).toBeTruthy();
+
+        await user.press(getByTestId("pressableContainer"));
+        expect(checkOnSelect).not.toBeTruthy();
+    });
+});
