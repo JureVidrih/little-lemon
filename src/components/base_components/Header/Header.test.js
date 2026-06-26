897332049285131b259923f814f6e4a7c712e506
Jure Vidrih <jurevidrih@gmail.com>
2026-06-26 02:12:45 +0200

    Implement tests for <Header />

---
+++ src/components/base_components/Header/Header.test.js
@@ -0,0 +1,40 @@
+import { render } from '@testing-library/react-native';
+
+import Header from './Header.tsx';
+
+import headerStyles, { fontSizes } from './HeaderStyles.ts';
+
+describe("Header component", () => {
+    it("should have rendered correctly", async () => {
+        const { getByTestId } = await render(<Header />);
+
+        expect(getByTestId("viewContainer")).toBeOnTheScreen();
+    });
+
+    it("should have rendered correctly with no props", async () => {
+        const { getByTestId } = await render(<Header />);
+
+        expect(getByTestId("viewContainer")).toHaveStyle({
+            ...headerStyles.container
+        });
+
+        expect(getByTestId("textContainer")).toHaveStyle({
+            ...headerStyles.headerText,
+            fontSize: fontSizes[1],
+            textAlign: "left"
+        });
+    });
+
+    it("should have at least one available header size to choose from", async () => {
+        expect(Object.keys(fontSizes).length > 0).toBeTruthy();
+    });
+
+    it("should respect sizeType prop and align prop", async () => {
+        const { getByTestId } = await render(<Header align="right" sizeType={Object.keys(fontSizes)[0]} />);
+
+        expect(getByTestId("textContainer")).toHaveStyle({
+            fontSize: fontSizes[Object.keys(fontSizes)[0]],
+            textAlign: "right"
+        });
+    });
+});
